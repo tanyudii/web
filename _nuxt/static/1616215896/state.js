412daba9a889962f503e601751482480a74abc55
tanyudii <y.hertanto17@gmail.com>
2021-03-20 11:51:53 +0700

    769c0c2

---
+++ _nuxt/static/1616215896/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616215896",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6c2b2b7d"}}}}(null,"\u002F"));
